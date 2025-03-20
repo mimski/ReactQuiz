248ec9a95dc6ad976e66ce1bae37b0fd89c61a2b
mimski <maria.bg.1989@gmail.com>
2025-03-21 00:36:09 +0200

    adding a results page, implementing the restart button

--- react-quiz/src/components/Quiz.js
+++ react-quiz/src/components/Quiz.js
@@ -1,5 +1,5 @@
 import { useContext } from "react";
-import Question from './Question'
+import Question from './Question';
 import {QuizContext} from "../contexts/quiz";
 
 const Quiz = () => {
@@ -7,11 +7,23 @@ const Quiz = () => {
     console.log("quizState", quizState);
     return (
         <div className="quiz">
+        {quizState.showResults && (
+            <div className="results">
+                <div className="congratulations">Congratulations</div>
+                <div className="results-info">
+                    <div>You have completed the quiz.</div>
+                    <div>You've got 4 of {quizState.questions.length}</div>
+                </div>
+                <div className="next-button" onClick={ () => dispatch({ type: "RESTART" }) }>Restart</div>
+            </div>
+        )}
+        {!quizState.showResults && (
             <div>
                 <div className="score">Question {quizState.currentQuestionIndex + 1}/{quizState.questions.length}</div>
                 <Question />
                 <div className="next-button" onClick={() => dispatch({type: "NEXT_QUESTION"})}>Next question</div>
             </div>
+        )}
         </div>
     )
 }
--- react-quiz/src/contexts/quiz.js
+++ react-quiz/src/contexts/quiz.js
@@ -4,12 +4,20 @@ import questions from "../data";
 const initialState = {
     currentQuestionIndex: 0,
     questions: questions,
+    showResults: false,
 };
 
 const reducer = (state, action) => {
-    if (action.type === 'NEXT_QUESTION') {
-        return {...state, currentQuestionIndex: state.currentQuestionIndex + 1};
+    if (action.type === "NEXT_QUESTION") {
+        const showResults = state.currentQuestionIndex === state.questions.length - 1;
+        const currentQuestionIndex = showResults ? state.currentQuestionIndex : state.currentQuestionIndex + 1;
+        return {...state, currentQuestionIndex: currentQuestionIndex, showResults: showResults};
     }
+
+    if (action.type === "RESTART") {
+        return initialState;
+    }
+
     return state;
 };
 
